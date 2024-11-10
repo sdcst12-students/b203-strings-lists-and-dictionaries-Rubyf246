580a78b40550db0dbc86ea4dd74955131c172a29
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2024-11-10 15:58:43 -0800

    task2 done

--- task2.py
+++ task2.py
@@ -3,10 +3,23 @@
 '''
 use a for loop to iterate through all possible integers to find the factors of 24
 '''
+
+def find_factors(x):
+    y = x + 1
+    factor_list = []
+    
+    for n in range(y):
+        if n != 0:
+           if  x%n == 0:
+               factor_list.append(n)
+    return factor_list        
+
+
 def main():
     factors = []
     myNumber = 24
-
+    factors = find_factors(myNumber)
+    print (factors)
 
 if __name__ == "__main__":
     main()
